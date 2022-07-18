15a1f5b0b19f4a137d88725b7951adc50d43b0a1
agesa3 <collinsagesa2000@gmail.com>
2022-07-19 00:59:25 +0300

    added readme

---
+++ app/src/test/java/com/agesadev/simpetestcases/ui/UserInputValidatorTest.kt
@@ -0,0 +1,18 @@
+package com.agesadev.simpetestcases.ui
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class UserInputValidatorTest {
+
+    @org.junit.jupiter.api.Test
+    fun validateName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun validateEmail() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun validateBitmap() {
+    }
+}
