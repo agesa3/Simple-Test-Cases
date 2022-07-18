04684b3166fe39129c6fb3f8010291ca6a9acb7d
agesa3 <collinsagesa2000@gmail.com>
2022-07-19 01:03:40 +0300

    added viewmodel and some unit tests

--- app/src/main/java/com/agesadev/simpetestcases/ui/MainActivity.kt
+++ app/src/main/java/com/agesadev/simpetestcases/ui/MainActivity.kt
@@ -1,12 +1,50 @@
 package com.agesadev.simpetestcases.ui
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.lifecycle.ViewModelProvider
 import com.agesadev.simpetestcases.R
+import com.agesadev.simpetestcases.databinding.ActivityMainBinding
+import com.agesadev.simpetestcases.model.UserBody
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var mainActivityViewModel: MainActivityViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-   }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        mainActivityViewModel = ViewModelProvider(this)[MainActivityViewModel::class.java]
+
+        binding.btnSubmit.setOnClickListener {
+            val email = binding.etEmail.text.toString()
+            val name = binding.etName.text.toString()
+            if (UserInputValidator.validateEmail(email).not()) {
+                return@setOnClickListener
+            }
+            if (UserInputValidator.validateName(name).not()) {
+                return@setOnClickListener
+            }
+            mainActivityViewModel.createUser(UserBody(name, email))
+        }
+        val getContent =
+            registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+                val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, uri)
+                if (UserInputValidator.validateBitmap(bitmap).not()) {
+                    return@registerForActivityResult
+                }
+                binding.ivImg.setImageBitmap(bitmap)
+            }
+
+        binding.ivImg.setOnClickListener {
+            getContent.launch("image/*")
+        }
+    }
 }
--- app/src/main/java/com/agesadev/simpetestcases/ui/UserInputValidator.kt
+++ app/src/main/java/com/agesadev/simpetestcases/ui/UserInputValidator.kt
@@ -1,4 +1,36 @@
 package com.agesadev.simpetestcases.ui
 
-class UserInputValidator {
+import android.graphics.Bitmap
+
+object UserInputValidator {
+
+    fun validateName(name: String): Boolean {
+        if (name.isEmpty()) {
+            return false
+        }
+        if (name.length > 20) {
+            return false
+        }
+
+        return true
+    }
+
+    fun validateEmail(email: String): Boolean {
+        if (email.isEmpty()) {
+            return false
+        }
+        if (email.contains("@").not()) {
+            return false
+        }
+        if (email.contains(".").not()) {
+            return false
+        }
+
+        return true
+    }
+
+    fun validateBitmap(bitmap: Bitmap): Boolean {
+        return bitmap.width > 100 && bitmap.height > 100
+    }
+
 }
