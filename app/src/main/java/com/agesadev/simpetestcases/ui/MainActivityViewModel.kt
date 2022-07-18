9686f3a6688520b95b5d222f0bc365985ea01202
agesa3 <collinsagesa2000@gmail.com>
2022-07-19 00:17:50 +0300

    done with viewmodle

--- app/src/main/java/com/agesadev/simpetestcases/ui/MainActivityViewModel.kt
+++ app/src/main/java/com/agesadev/simpetestcases/ui/MainActivityViewModel.kt
@@ -1,4 +1,21 @@
 package com.agesadev.simpetestcases.ui
 
-class MainActivityViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.agesadev.simpetestcases.BaseApplication
+import com.agesadev.simpetestcases.api.RetrofitObject
+import com.agesadev.simpetestcases.model.UserBody
+import kotlinx.coroutines.launch
+
+class MainActivityViewModel : ViewModel() {
+
+    val db = BaseApplication.INSTANCE.getAppDatabase()
+    val mainActivityRepo = MainActivityRepo(RetrofitObject.apiService, db)
+
+    fun createUser(userBody: UserBody) {
+        viewModelScope.launch {
+            val response = mainActivityRepo.createNewUser(userBody)
+            if (response.isSuccessful) mainActivityRepo.insertUserToDb(userBody)
+        }
+    }
 }
