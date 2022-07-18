00aff5234d2dfa036eafa8b1d65c7e3766d86278
agesa3 <collinsagesa2000@gmail.com>
2022-07-19 01:24:30 +0300

    passed all the unit test

--- app/src/test/java/com/agesadev/simpetestcases/ui/UserInputValidatorTest.kt
+++ app/src/test/java/com/agesadev/simpetestcases/ui/UserInputValidatorTest.kt
@@ -1,5 +1,6 @@
 package com.agesadev.simpetestcases.ui
 
+import com.agesadev.simpetestcases.ui.UserInputValidator.validateEmail
 import com.agesadev.simpetestcases.ui.UserInputValidator.validateName
 import junit.framework.TestCase.assertEquals
 import org.junit.Test
@@ -10,22 +11,33 @@ internal class UserInputValidatorTest {
     @Test
     fun `show an error n empty name input`() {
         val name = ""
-        assertEquals(false,validateName(name))
+        assertEquals(false, validateName(name))
     }
 
     @Test
-    fun `show success for valid name`(){
-        val name="Agesa"
+    fun `show success for valid name`() {
+        val name = "Agesa"
+        assertEquals(true, validateName(name))
 
     }
 
     @Test
-    fun validateEmail() {
-
+    fun `show error for an empty email field`() {
+        val email = ""
+        assertEquals(false, validateEmail(email))
     }
-
 
     @Test
-    fun validateBitmap() {
+    fun `show error for an invalid email`() {
+        val email = "test"
+        assertEquals(false, validateEmail(email))
     }
+
+    @Test
+    fun `show success for valid email`() {
+        val email = "test@gmail.com"
+        assertEquals(true, validateEmail(email))
+    }
+
+
 }
